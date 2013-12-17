853bd987bfeeb99398c5a5f2a67fef2db88cf739
momomodotcom <git@momomo.com>
2013-12-17 16:53:41 +0100

    no message

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 MoEV
 ====
+
+[MoEV.js](https://github.com/momomodotcom/MoEV/blob/master/src/MoEV.js "MoEV.js")
+
--- src/MoEV.js
+++ src/MoEV.js
@@ -1,10 +1,21 @@
 /**
- * No threading or listening involved meaning that other than a small memory footprint,
- * this library does not utilize any cpu power to determine when an event is fired or not.
+ * Copyright (C) 2013 momomo.com <opensource@momomo.com>
  *
- * TODO Consider key as optional if on is provided on listen and notify
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @Author Mohamed Seifeddine
+ * @Author Philip Nilsson
  */
-
 (function() {
         define ? define([], callback) : callback();
 
